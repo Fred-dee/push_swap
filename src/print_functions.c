eb9550aae22b2f699981cfe915195e9bd4764005
Fred Dilapisho <fred.dilapisho@gmail.com>
2018-07-23 14:40:31 +0200

    Printing functions separated and normed

--- src/print_functions.c
+++ src/print_functions.c
@@ -14,9 +14,9 @@
 
 void	print_rest(t_list *head_a, t_list *head_b, t_list *aterm, t_list *bterm)
 {
-	while (head_b != bterm || head_a != bterm)
+	while (head_b != bterm || head_a != aterm)
 	{
-		if (head_a != aterm)
+		if (head_a != aterm && head_a != NULL)
 		{
 			ft_putnbr(*((int *)head_a->content));
 			ft_putchar('\t');
@@ -24,199 +24,74 @@ void	print_rest(t_list *head_a, t_list *head_b, t_list *aterm, t_list *bterm)
 		}
 		else
 			ft_putchar('\t');
-		if (head_b !=  bterm)
+		if (head_b != bterm && head_b != NULL)
 		{
 			ft_putnbr(*((int *)head_b->content));
 			head_b = head_b->next;
 		}
 		ft_putchar('\n');
 	}
-	ft_putendl("_\t_");
-	ft_putendl("a\tb\n");
-}
-
-
-int		rotate_print(t_list *head_a, t_list *head_b, char *str)
-{
-	char	*colour1;
-	char	*colour2;
-	t_list	*tail_a;
-	t_list	*tail_b;
-
-	tail_a = ft_lstgettail(head_a);
-	tail_b = ft_lstgettail(head_b);
-	if (ft_strcmp(str, "ra") == 0 || ft_strcmp(str, "rb") == 0 || ft_strcmp(str, "rr") == 0)
+	if (head_a == NULL && head_b == NULL)
 	{
-		colour1 = ft_strdup(RED);
-		colour2 = ft_strdup(GREEN);
+		ft_putendl("_\t_");
+		ft_putendl("a\tb\n");
 	}
-	if (ft_strcmp(str, "ra") == 0)
-	{
-		if (head_a != NULL)
-		{
-			ft_putnbr_clr(colour1, *((int *)head_a->content));
-			if (head_b != NULL)
-				ft_putchar('\t');
-			else
-				ft_putchar('\n');
-			head_a = head_a->next;
-		}
-		if (head_b != NULL)
-		{
-			if(head_a == NULL)
-				ft_putchar('\t');
-			ft_putnbr_clr(WHITE, *((int *)head_b->content));
-			head_b = head_b->next;
-			ft_putchar('\n');
-		}
-		print_rest(head_a, head_b, tail_a, tail_b);
-		if (tail_a != NULL)
-		{
-			ft_putnbr_clr(colour2, *((int *)tail_a->content));
-			if(tail_b != NULL)
-				ft_putchar('\t');
-			else
-				ft_putchar('\n');
-		}
-		if (tail_b != NULL)
-		{
-			if(tail_a == NULL)
-				ft_putchar('\t');
-			ft_putnbr_clr(WHITE, *((int *)tail_b->content));
-			ft_putchar('\n');
-		}	
-		return (1);	
-	}
-	return (0);
 }
 
 int		swap_print(t_list *head_a, t_list *head_b, char *str)
 {
-	char	*colour1;
-	char	*colour2;
+	char	colour[4][8];
+	int		i;
 
-	if (ft_strcmp(str, "sa") == 0 || ft_strcmp(str, "sb") == 0 || ft_strcmp(str, "ss") == 0)
-	{
-		colour1 = ft_strdup(RED);
-		colour2 = ft_strdup(GREEN);
-	}
-	if (ft_strcmp(str, "sa") == 0 )
+	i = 0;
+	set_swap_colour(colour, str);
+	while (i < 4)
 	{
 		if (head_a != NULL)
 		{
-			print_single(colour1, head_b, *((int *)head_a->content));
+			print_single(colour[i], head_b, *((int *)head_a->content));
 			head_a = head_a->next;
 		}
+		i++;
 		if (head_b != NULL)
 		{
-			print_single(WHITE, head_a, *((int *)head_b->content));
+			print_single(colour[i], head_a, *((int *)head_b->content));
 			head_b = head_b->next;
 			ft_putchar('\n');
 		}
-		if (head_a != NULL)
-		{
-			print_single(colour2, head_b, *((int *)head_a->content));
-			head_a = head_a->next;
-		}
-		if (head_b != NULL)
-		{
-			print_single(WHITE, head_a, *((int *)head_b->content));
-			head_b = head_b->next;
-			ft_putchar('\n');
-		}
+		i++;
 	}
-	if (ft_strcmp(str, "sb") == 0)
-	{
-		if (head_a != NULL)
-		{
-			print_single(WHITE, head_b, *((int *)head_a->content));
-			head_a = head_a->next;
-		}
-		if (head_b != NULL)
-		{
-			print_single(colour1, head_a, *((int *)head_b->content));
-			head_b = head_b->next;
-			ft_putchar('\n');
-		}
-		if (head_a != NULL)
-		{
-			print_single(WHITE, head_b, *((int *)head_a->content));
-			head_a = head_a->next;
-		}
-		if (head_b != NULL)
-		{
-			print_single(colour2, head_a, *((int *)head_b->content));
-			head_b = head_b->next;
-			ft_putchar('\n');
-		}
-	}
-	if (ft_strcmp(str, "ss") == 0)
-	{
-		if (head_a != NULL)
-		{
-			print_single(colour1, head_b, *((int *)head_a->content));
-			head_a = head_a->next;
-		}
-		if (head_b != NULL)
-		{
-			print_single(colour1, head_a, *((int *)head_b->content));
-			head_b = head_b->next;
-			ft_putchar('\n');
-		}
-		if (head_a != NULL)
-		{
-			print_single(colour2, head_b, *((int *)head_a->content));
-			head_a = head_a->next;
-		}
-		if (head_b != NULL)
-		{
-			print_single(colour2, head_a, *((int *)head_b->content));
-			head_b = head_b->next;
-			ft_putchar('\n');
-		}
-	}
-	if (ft_strcmp(str, "sa") == 0 || ft_strcmp(str, "sb") == 0 || ft_strcmp(str, "ss") == 0)
-	{
-		print_rest(head_a, head_b, NULL, NULL);
-		return (1);
-	}
-	return (0);
+	print_rest(head_a, head_b, NULL, NULL);
+	return (1);
 }
 
 int		push_print(t_list *head_a, t_list *head_b, char *str)
 {
-	char	*colour1;
-	char	*colour2;
+	char	colour[2][8];
 
 	if (ft_strcmp(str, "pa") == 0)
 	{
-		colour1 = ft_strdup(GREEN);
-		colour2 = ft_strdup(RED);
+		ft_strcpy(colour[0], GREEN);
+		ft_strcpy(colour[1], RED);
 	}
 	if (ft_strcmp(str, "pb") == 0)
 	{
-		colour1 = ft_strdup(RED);
-		colour2 = ft_strdup(GREEN);
+		ft_strcpy(colour[0], RED);
+		ft_strcpy(colour[1], GREEN);
 	}
-	if (ft_strcmp(str, "pa") == 0 || ft_strcmp(str, "pb") == 0)
+	if (head_a != NULL)
 	{
-		if (head_a != NULL)
-		{
-			print_single(colour1, head_b, *((int *)head_a->content));
-			head_a = head_a->next;
-		}
-		if (head_b != NULL)
-		{
-			print_single(colour2, head_a, *((int *)head_b->content));
-			head_b = head_b->next;
-			ft_putchar('\n');
-		}
-		print_rest(head_a, head_b, NULL, NULL);
-		free(colour1);
-		free(colour2);
-		return (1);
+		print_single(colour[0], head_b, *((int *)head_a->content));
+		head_a = head_a->next;
 	}
-	return (0);	
+	if (head_b != NULL)
+	{
+		print_single(colour[1], head_a, *((int *)head_b->content));
+		head_b = head_b->next;
+		ft_putchar('\n');
+	}
+	print_rest(head_a, head_b, NULL, NULL);
+	return (1);
 }
 
 void	print_stacks_clr(t_stack *a, t_stack *b, char *str)
@@ -226,14 +101,17 @@ void	print_stacks_clr(t_stack *a, t_stack *b, char *str)
 
 	head_a = ft_stacktop(a);
 	head_b = ft_stacktop(b);
-	if (push_print(head_a, head_b, str) != 1)
-	{
-		if (swap_print(head_a, head_b, str) != 1)
-		{
-			if (rotate_print(head_a, head_b, str) != 1)
-				ft_putstr_clr(RED, "Invalid move selected\n");
-		}
-	}
+	if (ft_strcmp(str, "pa") == 0 || ft_strcmp(str, "pb") == 0)
+		push_print(head_a, head_b, str);
+	else if (ft_strcmp(str, "sa") == 0 || ft_strcmp(str, "sb") == 0
+		|| ft_strcmp(str, "ss") == 0)
+		swap_print(head_a, head_b, str);
+	else if (ft_strcmp(str, "ra") == 0 || ft_strcmp(str, "rb") == 0
+		|| ft_strcmp(str, "rr") == 0 || ft_strcmp(str, "rra") == 0 ||
+		ft_strcmp(str, "rrb") == 0 || ft_strcmp(str, "rrr") == 0)
+		rotate_print(head_a, head_b, str);
+	else
+		ft_putstr_clr(RED, "Invalid move selected\n");
 }
 
 void	print_stacks(t_stack *a, t_stack *b)
--- src/print_functions2.c
+++ src/print_functions2.c
@@ -12,11 +12,116 @@
 
 #include "../includes/push_swap.h"
 
+int		rotate_print(t_list *head_a, t_list *head_b, char *str)
+{
+	char	colour[4][8];
+	t_list	*tail_a;
+	t_list	*tail_b;
+
+	tail_a = ft_lstgettail(head_a);
+	tail_b = ft_lstgettail(head_b);
+	set_rotate_colour(colour, str);
+	if (head_a != NULL)
+	{
+		print_single(colour[0], head_b, *((int *)head_a->content));
+		head_a = head_a->next;
+	}
+	if (head_b != NULL)
+	{
+		print_single(colour[1], head_a, *((int *)head_b->content));
+		head_b = head_b->next;
+		ft_putchar('\n');
+	}
+	print_rest(head_a, head_b, tail_a, tail_b);
+	if (tail_a != NULL)
+		print_single(colour[2], tail_b, *((int *)tail_a->content));
+	if (tail_b != NULL)
+		print_single(colour[3], tail_a, *((int *)tail_b->content));
+	ft_putendl("\n_\t_\na\tb");
+	return (1);
+}
+
 void	print_single(char const *colour, t_list *head, int value)
 {
 	ft_putnbr_clr(colour, value);
-	if(head != NULL)
+	if (head != NULL)
 		ft_putchar('\t');
 	else
 		ft_putchar('\n');
+}
+
+void	set_rotate_colour_help(char colour[4][8], char *str)
+{
+	if (ft_strcmp(str, "rra") == 0)
+	{
+		ft_strcpy(colour[0], GREEN);
+		ft_strcpy(colour[1], WHITE);
+		ft_strcpy(colour[2], RED);
+		ft_strcpy(colour[3], WHITE);
+	}
+	if (ft_strcmp(str, "rrb") == 0)
+	{
+		ft_strcpy(colour[0], WHITE);
+		ft_strcpy(colour[1], GREEN);
+		ft_strcpy(colour[2], WHITE);
+		ft_strcpy(colour[3], RED);
+	}
+	if (ft_strcpy(str, "rrr") == 0)
+	{
+		ft_strcpy(colour[0], GREEN);
+		ft_strcpy(colour[1], GREEN);
+		ft_strcpy(colour[2], RED);
+		ft_strcpy(colour[3], RED);
+	}
+}
+
+void	set_rotate_colour(char colour[4][8], char *str)
+{
+	if (ft_strcmp(str, "ra") == 0)
+	{
+		ft_strcpy(colour[0], RED);
+		ft_strcpy(colour[1], WHITE);
+		ft_strcpy(colour[2], GREEN);
+		ft_strcpy(colour[3], WHITE);
+	}
+	if (ft_strcmp(str, "rb") == 0)
+	{
+		ft_strcpy(colour[0], WHITE);
+		ft_strcpy(colour[1], RED);
+		ft_strcpy(colour[2], WHITE);
+		ft_strcpy(colour[3], GREEN);
+	}
+	if (ft_strcpy(str, "rr") == 0)
+	{
+		ft_strcpy(colour[0], RED);
+		ft_strcpy(colour[1], RED);
+		ft_strcpy(colour[2], GREEN);
+		ft_strcpy(colour[3], GREEN);
+	}
+	set_rotate_colour_help(colour, str);
+}
+
+void	set_swap_colour(char colour[4][8], char *str)
+{
+	if (ft_strcmp(str, "sa") == 0)
+	{
+		ft_strcpy(colour[0], RED);
+		ft_strcpy(colour[1], WHITE);
+		ft_strcpy(colour[2], GREEN);
+		ft_strcpy(colour[3], WHITE);
+	}
+	if (ft_strcmp(str, "sb") == 0)
+	{
+		ft_strcpy(colour[0], WHITE);
+		ft_strcpy(colour[1], RED);
+		ft_strcpy(colour[2], WHITE);
+		ft_strcpy(colour[3], GREEN);
+	}
+	if (ft_strcpy(str, "ss") == 0)
+	{
+		ft_strcpy(colour[0], RED);
+		ft_strcpy(colour[1], RED);
+		ft_strcpy(colour[2], GREEN);
+		ft_strcpy(colour[3], GREEN);
+	}
 }
