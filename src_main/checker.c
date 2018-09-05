239f8bfec94ac20d686cf2c8544238da80fb0df2
Fred-Dee <fred.dilapisho@gmail.com>
2018-09-05 19:57:31 +0200

    apparently gnl leaks memory

--- src_main/checker.c
+++ src_main/checker.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <push_swap.h>
+#include "../includes/push_swap.h"
 
 static void	select_move(t_stack *a, t_stack *b, char *str)
 {
